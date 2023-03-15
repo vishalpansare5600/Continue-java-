075dfa9536addb7e09c2b13221c0f672552c5413
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-15 10:13:57 +0530

    Add files via upload

---
+++ continue/src/continueno.java
@@ -0,0 +1,17 @@
+
+public class continueno {
+
+	public static void main(String[] args) 
+	{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		if(i==5)
+		{
+			return;  //continue / break;
+		}
+		System.out.println("value of i="+i);
+	}
+	}
+
+}
